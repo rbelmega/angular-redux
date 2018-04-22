6b2d3adaa4fd53a83598e500fafd9547fd796d49
rbelmeha <belmega31@gmail.com>
2018-04-22 21:22:27 +0300

    remove router reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,18 +1,13 @@
 import {ActionReducerMap, MetaReducer} from '@ngrx/store';
-import {RouterReducerState, routerReducer} from '@ngrx/router-store';
-import {storeFreeze} from 'ngrx-store-freeze';
 import * as demo from '../components/demo/demo.reducer';
-
 
 export interface AppState {
   demo: demo.DemoState;
-  routerReducer?: RouterReducerState;
 }
 
 //storeFreeze
 export const metaReducers: Array<MetaReducer<AppState>> = [];
 
 export const reducers: ActionReducerMap<AppState> = {
-  demo: demo.reducer,
-  routerReducer,
+  demo: demo.reducer
 };
