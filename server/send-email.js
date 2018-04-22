604be7a136129d52a8d29b223f6d07c4dd307577
rbelmeha <belmega31@gmail.com>
2018-04-22 19:49:41 +0300

    added email sender

--- server/send-email.js
+++ server/send-email.js
@@ -11,14 +11,14 @@ module.exports = function sendEmail({ store, res }) {
     }
   });
 
-  // jsonfile.writeFileSync(file, store)
+  jsonfile.writeFileSync(file, store)
 
   const mailOptions = {
     from: process.env.TO_EMAIL,
     to: process.env.FROM_EMAIL,
     subject: "Hey, here is toy store",
     text:"Hey, here is toy store",
-    attachments: Buffer.from(store)
+    attachments: fs.readFileSync(file, 'utf8')
   };
 
   transporter.sendMail(mailOptions, function (err, info) {
